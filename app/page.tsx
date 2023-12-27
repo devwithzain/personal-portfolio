d36b0bd3c661f00aefdfba31cc5f7ce0f7fd316d
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-27 23:22:11 +0500

    PreLoad component rendered and added lenis scroll and 3 seconds wait for loader to show

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
+"use client";
+import { PreLoad } from "@components";
+import Lenis from "@studio-freight/lenis";
+import { useEffect, useState } from "react";
+import { AnimatePresence } from "framer-motion";
 export default function App() {
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		const lenis = new Lenis();
+
+		function raf(time: any) {
+			lenis.raf(time);
+			requestAnimationFrame(raf);
+		}
+
+		requestAnimationFrame(raf);
+
+		(() => {
+			setTimeout(() => {
+				setIsLoading(false);
+				document.body.style.cursor = "default";
+				window.scrollTo(0, 0);
+			}, 3000);
+		})();
+	}, []);
+
 	return (
-		<div>
-			<h1 className="text-[30rem]">Portfolio</h1>
-		</div>
+		<>
+			<AnimatePresence mode="wait">{isLoading && <PreLoad />}</AnimatePresence>
+			<div className="w-full h-[300svh] flex items-center justify-center">
+				<h1 className="text-[50px] uppercase font-bold">Home</h1>
+			</div>
+		</>
 	);
 }
