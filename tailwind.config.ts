c0cbb1164c23308a26a028d1b0c756e953bbde4c
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-02 11:42:34 +0500

    Added some fonts and animation folder in the content array

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './animation/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#00040f",
+        secondary: "#00f6ff",
       },
+      fontFamily: {
+        montreal: ["Montreal", "sans-serif"],
+        montrealThin: ["MontrealThin", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: '479px',
+      ss: '619px',
+      sm: '767px',
+      md: '1059px',
+      lg: '1199px',
+      xl: '1699px',
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
