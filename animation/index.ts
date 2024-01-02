213639979d144f7ba75367d3be22d79c313e8f93
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-02 11:47:01 +0500

    Exported different animation components

---
+++ animation/index.ts
@@ -0,0 +1,4 @@
+export { default as PreLoad } from './PreLoad';
+export { default as TextMask } from './TextMask';
+export { default as Word } from './Word';
+export { default as TextMarquee } from './TextMarquee';
