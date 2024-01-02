5dee312408a11e2eda070d8b4e25201637946ee6
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-02 11:47:57 +0500

    Exported different components and seperated animation component in animation folder

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,5 @@
-export { default as PreLoad } from './PreLoad';
+export { default as Home } from './Home';
+export { default as Navbar } from './Navbar';
+export { default as About } from './About';
+export { default as Hero } from './Hero';
+export { default as Project } from './Project';
