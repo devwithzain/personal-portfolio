6b4489d340fd4efddae0da4f18db399e05a4a2f5
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-04 22:00:59 +0500

    Exported Footer and Process components

--- components/index.ts
+++ components/index.ts
@@ -3,3 +3,5 @@ export { default as Navbar } from './Navbar';
 export { default as About } from './About';
 export { default as Hero } from './Hero';
 export { default as Project } from './Project';
+export { default as Process } from './Process';
+export { default as Footer } from './Footer';
