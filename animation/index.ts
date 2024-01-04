e4cc6e3dfdb46e94b476892b6bc53ce6a01f00d9
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-04 21:58:18 +0500

    Exported sticky and parallax components

--- animation/index.ts
+++ animation/index.ts
@@ -2,3 +2,5 @@ export { default as PreLoad } from './PreLoad';
 export { default as TextMask } from './TextMask';
 export { default as Word } from './Word';
 export { default as TextMarquee } from './TextMarquee';
+export { default as Parallax } from './Parallax';
+export { default as Sticky } from './Sticky';
