adf75cc2bafb72e6f165fd03925bb176b85ffd27
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-04 21:58:48 +0500

    Sticky component completed

---
+++ animation/Sticky.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useEffect } from "react";
+import { motion, useMotionValue, useSpring } from "framer-motion";
+
+export default function Sticky() {
+	const cursorSize = 20;
+	const mouse = {
+		x: useMotionValue(0),
+		y: useMotionValue(0),
+	};
+
+	const smoothOptions = { damping: 20, stiffness: 300, mass: 0.5 };
+	const smoothMouse = {
+		x: useSpring(mouse.x, smoothOptions),
+		y: useSpring(mouse.y, smoothOptions),
+	};
+
+	const manageMouseMove = (e: any) => {
+		const { clientX, clientY } = e;
+		mouse.x.set(clientX - cursorSize / 2);
+		mouse.y.set(clientY - cursorSize / 2);
+	};
+	useEffect(() => {
+		window.addEventListener("mousemove", manageMouseMove);
+		return () => {
+			window.removeEventListener("mousemove", manageMouseMove);
+		};
+	});
+
+	return (
+		<motion.div
+			className="w-[15px] h-[15px] fixed rounded-[100%] bg-[#202020] pointer-events-none z-10"
+			style={{
+				left: smoothMouse.x,
+				top: smoothMouse.y,
+			}}></motion.div>
+	);
+}
