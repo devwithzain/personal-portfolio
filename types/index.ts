b95cae8ac9914daa9e3ee6e0e8a26b769af38a17
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-06 23:01:33 +0500

    Exported different components types

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,5 @@
 import { MotionValue } from "framer-motion";
+import { StaticImageData } from "next/image";
 
 export type TParagraphProps = {
     paragraph: string;
@@ -8,4 +9,31 @@ export type TWordProps = {
     children: string;
     progress: MotionValue<number>;
     range: number[];
+};
+
+export type TperspectiveTextProps = {
+    children: React.ReactNode;
+};
+
+
+export type TmodalProps = {
+    modal: {
+        active: boolean;
+        index: number;
+    };
+    projects: {
+        img: StaticImageData;
+        color: string;
+    }[];
+};
+
+export type TprojectProps = {
+    title: string,
+    index: number,
+    setModal: (modal: { active: boolean; index: number; }) => void;
+};
+
+export type TlogoMarqueeProps = {
+    children: React.ReactNode;
+    baseVelocity: number;
 };
