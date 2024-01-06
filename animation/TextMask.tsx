aa3d626e380e7b3e160a8bbbb694fe82b9710da9
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-06 23:02:58 +0500

    TextMask component completed

---
+++ animation/TextMask.tsx
@@ -0,0 +1,29 @@
+import { animation } from "@motion";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+export default function MaskText({ children }: { children: string[] }) {
+	const { ref, inView } = useInView({
+		threshold: 0.75,
+	});
+
+	return (
+		<div
+			ref={ref}
+			className="">
+			{children.map((phrase, index) => (
+				<div
+					key={index}
+					className="overflow-hidden">
+					<motion.p
+						custom={index}
+						variants={animation}
+						initial="initial"
+						animate={inView ? "enter" : ""}>
+						{phrase}
+					</motion.p>
+				</div>
+			))}
+		</div>
+	);
+}
