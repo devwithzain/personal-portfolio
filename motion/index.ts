cd1e8fb8a2a9678a713e6d800c0da31ee147f554
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-06 23:00:19 +0500

    Exported different animation constants

--- motion/index.ts
+++ motion/index.ts
@@ -26,17 +26,33 @@ export const animation = {
         transition: {
             duration: 0.75,
             ease: [0.33, 1, 0.68, 1],
-            delay: 1,
+            delay: 0.5,
         },
     }),
 };
 
 export const navVarients = {
     initial: { opacity: 0, y: "-100%" },
-    vissible: { opacity: 1, y: "0%", transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 1 } }
+    vissible: { opacity: 1, y: "0%", transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.5 } }
 };
 
 export const heroVarients = {
     initial: { y: "100%", opacity: 0 },
-    vissible: { y: "0%", opacity: 1, transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 1 } }
+    vissible: { y: "0%", opacity: 1, transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1], delay: 0.5 } }
+};
+
+export const scaleAnimation = {
+    initial: { scale: 0, x: "-50%", y: "-50%" },
+    enter: {
+        scale: 1,
+        x: "-50%",
+        y: "-50%",
+        transition: { duration: 0.4, ease: [0.76, 0, 0.24, 1] },
+    },
+    closed: {
+        scale: 0,
+        x: "-50%",
+        y: "-50%",
+        transition: { duration: 0.4, ease: [0.32, 0, 0.67, 0] },
+    },
 };
