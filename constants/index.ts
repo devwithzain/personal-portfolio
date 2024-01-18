7c530d98f5423488601c7d4600af758556643551
Zain Ali <ZainSoftwear11@gmail.com>
2024-01-18 22:32:05 +0500

    Exported different components constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,28 @@
+import { brastoolStore, budweiser, buzzFedd, c2s, forbes, locomotive, macys, mensHealth, mrBeast, officestudio, silencio, threeM } from "@public";
+
+export const project = [
+    {
+        title: "C2 Montreal",
+        img: c2s,
+        color: "#000000",
+    },
+    {
+        title: "Office Studio",
+        img: officestudio,
+        color: "#8C8C8C",
+    },
+    {
+        title: "Locomotive",
+        img: locomotive,
+        color: "#EFE8D3",
+    },
+    {
+        title: "Silencio",
+        img: silencio,
+        color: "#706D63",
+    },
+];
+
 export const words = [
     "Hello",
     "Bonjour",
@@ -7,4 +32,114 @@ export const words = [
     "Hallå",
     "Guten tag",
     "Assalamu Alaikum",
+];
+
+export const paragraph =
+    "I’m a multi-disciplinary designer and Webflow developer, passionate about crafting ideas, visuals, and typography into unforgettable digital experiences.  With a genuine belief in the transformative power of design, I am constantly exploring the vast creative world, from architecture, art, graphic design, illustration, motion design, and UX design. My journey began in architecture, however, my passion for limitless creativity led me to explore the endless possibilities that creative development has to offer.";
+
+export const images = [
+    "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg",
+    "5.jpg",
+    "6.jpg",
+    "7.jpg",
+    "8.jpg",
+    "9.jpg",
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+];
+
+
+
+export const processItem = [
+    {
+        id: "1",
+        title: "Art",
+        subTitle: "Direction",
+        para: "My process starts by predicting a crafting a visual narrative for the project, making it memorable, striking, and beautiful. To ensure a successful outcome, I place great importance on deeply understanding the project brief and client needs. This involves identifying project goals, functional specifications, and conducting thorough research to gain insights into competitors and target audiences."
+    },
+    {
+        id: "2",
+        title: "Digital",
+        subTitle: "Design",
+        para: "During the design phase, after establishing the desired mood and tone in the art direction stage, the mission is to connect the dots and explore different design iterations. I work to create an easy-to-use and enjoyable experience that highlights the unique characteristics of each project while maintaining an elegant and satisfying user interface."
+    },
+    {
+        id: "3",
+        title: "Webflow",
+        subTitle: "Development",
+        para: "Webflow development lies at the core of what I offer, with a focus on custom design tailored to your specific needs, setting your business apart from the competition. The main objective is to deliver a responsive website that is performant, pixel-perfect, and offers a fluid user experience with carefully crafted animations and interactions."
+    },
+    {
+        id: "4",
+        title: "Interaction",
+        subTitle: "Design",
+        para: "I firmly believe that motion and interactivity are essential components of the digital environment, serving as a vital link between the audience and the product. Even the smallest and most subtle motions or micro-interactions have the power to elevate the overall user experience to new heights effortlessly."
+    },
+];
+
+export const TfooterItem = [
+    {
+        id: 1,
+        href: "https://www.linkedin.com/zainalisiddiqui",
+        title: "Linkedin"
+    },
+    {
+        id: 2,
+        href: "https://www.linkedin.com/zainalisiddiqui",
+        title: "Github"
+    },
+    {
+        id: 3,
+        href: "",
+        title: "©2024"
+    },
+    {
+        id: 4,
+        href: "https://www.linkedin.com/zainalisiddiqui",
+        title: "Instagram"
+    },
+    {
+        id: 5,
+        href: "https://www.linkedin.com/zainalisiddiqui",
+        title: "Twitter"
+    },
+];
+
+export const skillsData = [
+    {
+        id: 1,
+        src: threeM
+    },
+    {
+        id: 2,
+        src: brastoolStore
+    },
+    {
+        id: 3,
+        src: budweiser
+    },
+    {
+        id: 4,
+        src: buzzFedd
+    },
+    {
+        id: 5,
+        src: forbes
+    },
+    {
+        id: 6,
+        src: macys
+    },
+    {
+        id: 7,
+        src: mensHealth
+    },
+    {
+        id: 8,
+        src: mrBeast
+    },
 ];
