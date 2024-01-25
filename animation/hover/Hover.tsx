b2b42023a6e05d03649470561cbf46fa549c4e8d
Zain <ZainSoftwear11@gmail.com>
2024-01-25 21:47:27 +0500

    Completed

---
+++ animation/hover/Hover.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useState } from "react";
+import Modal from "./modal/Modal";
+import { project } from "@constants";
+import Project from "./project/Project";
+export default function Hover() {
+	const [modal, setModal] = useState({ active: false, index: 0 });
+	return (
+		<main className="flex h-screen items-center justify-center">
+			<div className="w-full flex flex-col items-center justify-center">
+				{project.map((project, index) => {
+					return (
+						<Project
+							index={index}
+							title={project.title}
+							setModal={setModal}
+							key={index}
+						/>
+					);
+				})}
+			</div>
+			<Modal
+				modal={modal}
+				projects={project}
+			/>
+		</main>
+	);
+}
