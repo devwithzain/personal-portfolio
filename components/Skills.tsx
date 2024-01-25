5c73eb0a76ae78a65bd2245545aea546aced054f
Zain <ZainSoftwear11@gmail.com>
2024-01-25 21:47:50 +0500

    Completed

---
+++ components/Skills.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import { Marquee } from "@animation";
+import { skillsData } from "@constants";
+
+export default function Skills() {
+	return (
+		<section className="my-[100rem]">
+			<Marquee baseVelocity={-1}>
+				<div className="flex m-0 gap-x-[40px] items-center">
+					{skillsData.map((item) => (
+						<div
+							className="w-[200px] h-[100]"
+							key={item.id}>
+							<Image
+								src={item.src}
+								alt="asd"
+								width={200}
+								height={100}
+								className="w-[200px] h-[100px]"
+							/>
+						</div>
+					))}
+				</div>
+			</Marquee>
+			<Marquee baseVelocity={1}>
+				<div className="flex m-0 gap-x-[40px] items-center">
+					{skillsData.map((item) => (
+						<div
+							className="w-[200px] h-[100]"
+							key={item.id}>
+							<Image
+								src={item.src}
+								alt="asd"
+								width={200}
+								height={100}
+								className="w-[200px] h-[100px]"
+							/>
+						</div>
+					))}
+				</div>
+			</Marquee>
+		</section>
+	);
+}
