c95ac7b4bd0f7ec53525603705ca3e5c0a5bd0b4
Zain <ZainSoftwear11@gmail.com>
2024-01-25 21:45:59 +0500

    Completed

---
+++ animation/Column.tsx
@@ -0,0 +1,27 @@
+"use client";
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { TparallaxProps } from "@types";
+export default function Column({ y, images }: TparallaxProps) {
+	return (
+		<motion.div
+			className="relative h-full w-[25%] min-w-[250rem] flex flex-col gap-[2vw] odd:top-[-45%] even:top-[-95%] last:top-[-75%]"
+			style={{ y }}>
+			{images.map((src, i) => {
+				return (
+					<div
+						key={i}
+						className="h-full w-full relative rounded-[1vw] overflow-hidden">
+						<Image
+							className="object-cover"
+							src={`/${src}`}
+							alt="image"
+							fill
+							sizes="true"
+						/>
+					</div>
+				);
+			})}
+		</motion.div>
+	);
+}
