0a9d070f6b93bceb6bd19f450d599bea49c75b8e
Zain <ZainSoftwear11@gmail.com>
2024-01-25 21:49:36 +0500

    Completed

---
+++ components/Home.tsx
@@ -0,0 +1,21 @@
+import { Parallax, Sticky } from "@animation";
+import { About, Footer, Hero, Process, Project, Skills } from "@components";
+
+export default function Home() {
+	return (
+		<>
+			<div className="sm:hidden block">
+				<Sticky />
+			</div>
+			<div className="px-[50rem]">
+				<Hero />
+				<About />
+			</div>
+			<Project />
+			<div className="px-[50rem]">{<Process />}</div>
+			<Parallax />
+			<Skills />
+			<div className="px-[50rem]">{<Footer />}</div>
+		</>
+	);
+}
