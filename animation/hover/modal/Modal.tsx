f582ccca9a2bb6fae4ef2e78095663e514f99604
Zain <ZainSoftwear11@gmail.com>
2024-01-25 21:47:32 +0500

    Completed

---
+++ animation/hover/modal/Modal.tsx
@@ -0,0 +1,101 @@
+"use client";
+import gsap from "gsap";
+import Image from "next/image";
+import { TmodalProps } from "@types";
+import { motion } from "framer-motion";
+import { scaleAnimation } from "@motion";
+import { useRef, useEffect } from "react";
+
+export default function Modal({ modal, projects }: TmodalProps) {
+	const { active, index } = modal;
+	const modalContainer = useRef(null);
+	const cursor = useRef(null);
+	const cursorLabel = useRef(null);
+
+	useEffect(() => {
+		//Move Container
+		let xMoveContainer = gsap.quickTo(modalContainer.current, "left", {
+			duration: 0.8,
+			ease: "power3",
+		});
+		let yMoveContainer = gsap.quickTo(modalContainer.current, "top", {
+			duration: 0.8,
+			ease: "power3",
+		});
+		//Move cursor
+		let xMoveCursor = gsap.quickTo(cursor.current, "left", {
+			duration: 0.5,
+			ease: "power3",
+		});
+		let yMoveCursor = gsap.quickTo(cursor.current, "top", {
+			duration: 0.5,
+			ease: "power3",
+		});
+		//Move cursor label
+		let xMoveCursorLabel = gsap.quickTo(cursorLabel.current, "left", {
+			duration: 0.45,
+			ease: "power3",
+		});
+		let yMoveCursorLabel = gsap.quickTo(cursorLabel.current, "top", {
+			duration: 0.45,
+			ease: "power3",
+		});
+
+		window.addEventListener("mousemove", (e) => {
+			const { pageX, pageY } = e;
+			xMoveContainer(pageX);
+			yMoveContainer(pageY);
+			xMoveCursor(pageX);
+			yMoveCursor(pageY);
+			xMoveCursorLabel(pageX);
+			yMoveCursorLabel(pageY);
+		});
+	}, []);
+
+	return (
+		<>
+			<motion.div
+				ref={modalContainer}
+				variants={scaleAnimation}
+				initial="initial"
+				animate={active ? "enter" : "closed"}
+				className="h-[350rem] w-[400rem] absolute bg-white overflow-hidden pointer-events-none flex items-center justify-center">
+				<div
+					style={{ top: index * -100 + "%" }}
+					className="h-full w-full absolute transition-all duration-500 ease-[0.76, 0, 0.24, 1]">
+					{projects.map((project, index) => {
+						const { img, color } = project;
+						return (
+							<div
+								className="h-full w-full flex items-center justify-center"
+								style={{ backgroundColor: color }}
+								key={`modal_${index}`}>
+								<Image
+									src={img}
+									width={300}
+									height={0}
+									alt="image"
+									className="h-auto w-auto"
+								/>
+							</div>
+						);
+					})}
+				</div>
+			</motion.div>
+			<motion.div
+				ref={cursor}
+				className="w-[80rem] h-[80rem] rounded-[50%] bg-[#455ce9] text-white absolute z-10 flex items-center justify-center text-[14rem] font-light pointer-events-none"
+				variants={scaleAnimation}
+				initial="initial"
+				animate={active ? "enter" : "closed"}></motion.div>
+			<motion.div
+				ref={cursorLabel}
+				className="w-[80rem] h-[80rem] rounded-[50%] bg-[#455ce9] text-white absolute z-10 flex items-center justify-center text-[14rem] font-light pointer-events-none"
+				variants={scaleAnimation}
+				initial="initial"
+				animate={active ? "enter" : "closed"}>
+				View
+			</motion.div>
+		</>
+	);
+}
