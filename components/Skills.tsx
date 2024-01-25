33dcbd98bc891888bcc24bac8182bb331cebaab8
Zain <ZainSoftwear11@gmail.com>
2024-01-25 22:19:44 +0500

    Completed

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -5,40 +5,42 @@ import { skillsData } from "@constants";
 export default function Skills() {
 	return (
 		<section className="my-[100rem]">
-			<Marquee baseVelocity={-1}>
-				<div className="flex m-0 gap-x-[40px] items-center">
-					{skillsData.map((item) => (
-						<div
-							className="w-[200px] h-[100]"
-							key={item.id}>
-							<Image
-								src={item.src}
-								alt="asd"
-								width={200}
-								height={100}
-								className="w-[200px] h-[100px]"
-							/>
-						</div>
-					))}
-				</div>
-			</Marquee>
-			<Marquee baseVelocity={1}>
-				<div className="flex m-0 gap-x-[40px] items-center">
-					{skillsData.map((item) => (
-						<div
-							className="w-[200px] h-[100]"
-							key={item.id}>
-							<Image
-								src={item.src}
-								alt="asd"
-								width={200}
-								height={100}
-								className="w-[200px] h-[100px]"
-							/>
-						</div>
-					))}
-				</div>
-			</Marquee>
+			<div className="w-full flex flex-col gap-y-[50px]">
+				<Marquee baseVelocity={-1}>
+					<div className="flex m-0 gap-x-[40px] items-center">
+						{skillsData.map((item) => (
+							<div
+								className="w-[200px] h-[100]"
+								key={item.id}>
+								<Image
+									src={item.src}
+									alt="asd"
+									width={200}
+									height={100}
+									className="w-[200px] h-[100px]"
+								/>
+							</div>
+						))}
+					</div>
+				</Marquee>
+				<Marquee baseVelocity={1}>
+					<div className="flex m-0 gap-x-[40px] items-center">
+						{skillsData.map((item) => (
+							<div
+								className="w-[200px] h-[100]"
+								key={item.id}>
+								<Image
+									src={item.src}
+									alt="asd"
+									width={200}
+									height={100}
+									className="w-[200px] h-[100px]"
+								/>
+							</div>
+						))}
+					</div>
+				</Marquee>
+			</div>
 		</section>
 	);
 }
