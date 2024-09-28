1309615b78f70004aedc51a6cdb43898fb773693
devwithzain <zainsoftwear11@gmail.com>
2024-09-28 18:07:47 +0500

    modified

--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,6 @@ export { default as Hero } from './Hero';
 export { default as Project } from './Project';
 export { default as Process } from './Process';
 export { default as Footer } from './Footer';
-export { default as Skill } from './skill';
+export { default as Skill } from './Skill';
 export { default as Testimonials } from './Testimonials';
 export { default as Menu } from './Side-Menu/SideHome';
